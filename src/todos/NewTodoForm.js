58695d489615dc338a8d4c0eaba9f84f70f9e491
raustime <raustime@gmail.com>
2020-08-24 15:11:28 +0300

    first build of app

---
+++ src/todos/NewTodoForm.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import {connect} from 'react-redux';
+import {createTodo} from './actions';
+import './NewTodoForm.css';
+
+const NewTodoForm=({todos, onCreatePressed})=>{
+    const [inputValue, setInputValue]=useState('');
+    return (
+        <div className="new-todo-form">
+            <input 
+                className="new-todo-input" 
+                type="text"
+                placeholder="type your new todo here"
+                value={inputValue}
+                onChange={(e)=>setInputValue(e.target.value)}
+            />
+            <button 
+                className="new-todo-button"
+                onClick={()=>{
+                    const isDuplicateText=
+                        todos.some(todo=>todo.text===inputValue);
+                    if(!isDuplicateText) {
+                        onCreatePressed(inputValue);
+                        setInputValue('')
+                    }
+                    
+                }}
+            >Create Todo
+            </button>
+        </div>
+    );
+};
+const mapStateToProps=state=>({
+    todos: state.todos
+});
+
+const mapDispatchToProps=dispatch=>({
+    onCreatePressed: text=>dispatch(createTodo(text))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewTodoForm);
