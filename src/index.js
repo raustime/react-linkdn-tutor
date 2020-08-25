d16e89035748f69497b45d7c2baaa5a7dc8bc4dd
poweruser <C:\Users\1\AppData\Roaming\The Bat!>
2020-08-25 17:17:43 +0300

    Add redux-persist

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {configureStore} from './store';
+import {persistStore} from 'redux-persist';
+import {PersistGate} from 'redux-persist/lib/integration/react';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const store=configureStore();
+const persistor=persistStore(store);
 ReactDOM.render(
-  <Provider store={configureStore()}>
-    <App />
+  <Provider store={store}>
+    <PersistGate 
+       loading={<div>Loading...</div>}
+       persistor={persistor}
+    >
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
